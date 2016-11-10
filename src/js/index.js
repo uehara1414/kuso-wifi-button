839a7c18780a830099c60d72da3d17b2ee43125c
Isaac <r.sato7690@me.com>
2016-11-10 21:07:50 +0900

    Add js file and Fix icon

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+var counter; //countを表示するエリア
+var count = 0; //クソWiFiボタンを押された回数
+
+window.onload = function() {
+  counter = document.getElementById('counter');
+};
+
+/**
+ * クソWiFiボタンを押されたら実行される関数。
+ */
+function kusoButton() {
+  count++;
+  counter.innerText = count;
+}
